3062758d22602b5481ba602e1e5d58831214902f
Autodrop <matthijs_pijpker@hotmail.com>
2020-06-19 13:48:17 +0200

    id invullen werkt

--- app.js
+++ app.js
@@ -17,16 +17,25 @@ input.addEventListener("keyup", function (event) {
 
 /* const YOUTUBE_API_KEY = "AIzaSyDKm9l4BiqkUY_PjA_YYWfhCWNgA45-__g"; */
 
-$.getJSON(
-  "https://www.googleapis.com/youtube/v3/videos?part=statistics&id=" + ["bvim4rsNHkQ"] + "&key=AIzaSyDoPDFwoLr0xAK66w82LqrMqfAFnfd-jX0",
+/* var inputVal = document.getElementById("inputID").value; */
+  
+
+
+
+ function setID(){
+  $.getJSON(
+  "https://www.googleapis.com/youtube/v3/videos?part=statistics&id=" + inputID.value + "&key=AIzaSyDoPDFwoLr0xAK66w82LqrMqfAFnfd-jX0",
 
   function (data) {
     console.log(data);
 
-    /*  var views = data.items[0].statistics.viewCount; */
-
-    /* $(".views").append(data.items[0].statistics.viewCount); */
-
     $(".inputViews").val(data.items[0].statistics.viewCount);
-  }
-);
+  });
+  console.log(inputID.value);  
+}
+
+
+
+
+
+
